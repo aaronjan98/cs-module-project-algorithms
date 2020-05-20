990fb1ac15363d108a594789a8024c79f0a1c5b3
aaronjan <aaronjan98@gmail.com>
2020-05-20 14:49:06 -0700

    completed moving_zeros function

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,20 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
-
+    new_arr = []
+    count = 0
+    for i in range(len(arr)):
+        print(i, arr[i])
+        if arr[i] > 0 or arr[i] < 0:
+            print('in', arr[i])
+            new_arr.append(arr[i])
+        else:
+            count += 1
+    for i in range(count):
+        new_arr.append(0)
+    print(new_arr)
+    arr = new_arr
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,30 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    temp = []
+    temp.append(arr[0])
 
-    pass
+    print(temp)
 
+    if not arr[1]:
+        return temp[0]
 
+    for el in range(1, len(arr)):
+        # print('el')
+        if temp[0]:
+            for i in range(0, len(temp)):
+                # print(arr[el], temp[i])
+                if arr[el] == temp[i]:
+                    # print(i)
+                    temp.pop(i)
+                    temp.append(arr[el+1])
+                else:
+                    temp.append(arr[el])
+    print(temp)
+    return temp[0]
+    
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 9, 11, 12, 11]
 
     print(f"The odd-number-out is {single_number(arr)}")
