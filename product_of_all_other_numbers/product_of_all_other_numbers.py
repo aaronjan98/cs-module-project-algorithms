1daa6c1be4591861c7c2c9ca2f269f397c07e1d3
aaronjan <aaronjan98@gmail.com>
2020-05-21 07:31:37 -0700

    Update product_of_all_other_numbers.py

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,10 +3,61 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    new_arr = []
+    n = len(arr)
+    # Base case 
+    if(n == 1): 
+        print(0) 
+        return
+          
+    # Allocate memory for temporary arrays left[] and right[]  
+    left = [0]*n  
+    right = [0]*n  
+  
+    # Allocate memory for the product array  
+    prod = [0]*n  
+  
+    # Left most element of left array is always 1  
+    left[0] = 1
+  
+    # Rightmost most element of right array is always 1  
+    right[n - 1] = 1
+  
+    # Construct the left array  
+    for i in range(1, n):  
+        left[i] = arr[i - 1] * left[i - 1]  
+  
+    # Construct the right array  
+    for j in range(n-2, -1, -1):  
+        right[j] = arr[j + 1] * right[j + 1]  
+  
+    # Construct the product array using  
+    # left[] and right[]  
+    for i in range(n):  
+        prod[i] = left[i] * right[i]  
+  
+    # print the constructed prod array  
+    for i in range(n):  
+        # print(prod[i], end =' ')
+        new_arr.append(prod[i])
 
-    pass
-
+    return new_arr
+    '''
+    #first attemp
+    global total_mult
+    total_mult = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] == 0:
+            break
+        print(total_mult)
+        total_mult *= arr[i]
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            break
+        arr[i] = total_mult/arr[i]
+        # print(i)
+    return arr
+    '''
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
